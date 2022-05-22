4fdbb7dbb509964f26ebf238b3ede45783570fec
Anatoly Stolbov <stolbov1@gmail.com>
2022-05-22 10:57:45 +0300

    export added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 import random
+import os
 
 class MelodyWriter():
     def __init__(self):
@@ -17,6 +18,7 @@ class MelodyWriter():
             print("!!! error")
 
     def generate_variants(self):
+        # position steps dupliceted to let program do less jumps
         v = [*self.stable, self.position + 1,self.position + 1,self.position + 1, self.position - 1,self.position - 1,self.position - 1]
         return v
 
@@ -48,13 +50,18 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("-s", "--scale", dest="scale", default="b c' d' e' f' g' a'")
     parser.add_argument("-l", "--length", dest="len", default=100)
-    parser.add_argument("-o", "--output", dest="len", default="out.txt")
+    parser.add_argument("-o", "--output", dest="output", default="out.txt")
+    parser.add_argument('--export', action='store_true')
     args = parser.parse_args()
     writer = MelodyWriter()
     writer.notes = args.scale.split(" ")
-    writer.notes = args.scale.split(" ")
+    writer.filename = args.output
+    writer.write(args.len)
 
-    writer.write(100)
+    if (args.export):
+        print(f"export {args.output}")
+        os.system(f"lilypond --png {args.output}")
+
     return 1
 
 if __name__ == "__main__":
